e31135ebdbc0feafdc79f5dbf1a4726c5d95d675
Pearl Khin <pearlkhin@Pearls-Mac-mini.local>
2024-12-11 15:49:48 +0800

    feat: added CartItems

--- src/store/cart-slice.ts
+++ src/store/cart-slice.ts
@@ -1,7 +1,7 @@
 import {createSlice, type PayloadAction} from '@reduxjs/toolkit';
 
 
-type CartItem = {
+export type CartItem = {
     id: string;
     title: string;
     price: number;
@@ -21,21 +21,21 @@ export const cartSlice = createSlice({
         addToCart( state, action: PayloadAction<{ id: string; title: string; price: number}> ){
             const itemIndex = state.items.findIndex( (item) => item.id === action.payload.id );
             // if itemIndex have found, add quantity
-            if(itemIndex > 0){
-                state.items[itemIndex].quantity++;
+            if(itemIndex >= 0){
+                state.items[itemIndex].quantity ++;
             }else{
                 state.items.push({...action.payload, quantity: 1})
             }
         },
-        removeFromCart(state, action: PayloadAction<{id: string}>){
-            const itemIndex = state.items.findIndex((item) => item.id === action.payload.id);
+        removeFromCart(state, action: PayloadAction<string>){
+            const itemIndex = state.items.findIndex((item) => item.id === action.payload);
 
             // if found item quantity is 1 , remove the item
             if(state.items[itemIndex].quantity === 1){
                 state.items.splice(itemIndex, 1);
             }else{
                 // only deduct quantity --
-                state.items[itemIndex].quantity--;
+                state.items[itemIndex].quantity --;
 
             }
         }
